16a98ec2473c1402a4eef7f376b6d6db81567952
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-03-07 16:31:27 +0100

    Display Game's Name

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import * as core from "express-serve-static-core";
-import { Db } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 import nunjucks from "nunjucks";
 
 export function makeApp(db: Db): core.Express {
+  const databaseUrl = process.env.MONGO_URL || "";
+  const client = new MongoClient(databaseUrl);
   const app = express();
 
   nunjucks.configure("views", {
@@ -19,6 +21,15 @@ export function makeApp(db: Db): core.Express {
 
   app.get("/plateform", (request: Request, response: Response) => {
     response.render("platform");
+  });
+
+  app.get("/home", (request: Request, response: Response) => {
+    client.connect().then(async () => {
+      const database = client.db();
+      const games = await database.collection("games").find().toArray();
+      // console.log(games);
+      response.render("home", { games: games });
+    });
   });
 
   return app;
